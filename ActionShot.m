9c18b0e14c93a68d47bf2e88d1de01f39eed6002
DarkMatterMatt <mattmoran2000@gmail.com>
2019-08-26 23:24:50 +1200

    Update ActionShot.m
    More optimisations

--- ActionShot.m
+++ ActionShot.m
@@ -4,20 +4,21 @@ function output = ActionShot(images)
     [numY, numX, ~] = size(images{1});
     output = zeros(numY, numX, 3, 'uint8');
     
-    % convert to a 4D matrix
+    % convert to a 4D matrix - looks like
+    %   [Y, X, RGB, N] where N is the image number
     images = cat(4, images{:});
     
     images = double(images);
     medianImage = double(gMedianImage);
+    
+    subtractedImages = images - medianImage;
+    distances = sum(subtractedImages.^2, 3);
+    
+    [~, indexes] = max(distances, [], 4);
 
     for y = 1:numY
         for x = 1:numX
-            p = images(y, x, :, :);
-            m = medianImage(y, x, :);
-            
-            distances = sum((p-m).^2, 4);
-            [~, index] = max(distances);
-            output(y, x, :) = p(1, 1, :, index);
+            output(y, x, :) = images(y, x, :, indexes(y, x));
         end
     end
 end
