ff557b97be125d2710f49cc72f3014774b8a7dbc
DarkMatterMatt <mattmoran2000@gmail.com>
2019-08-27 13:28:09 +1200

    Add comments to ActionShot

--- ActionShot.m
+++ ActionShot.m
@@ -6,14 +6,19 @@ function output = ActionShot(images)
     images = cat(4, images{:});
     [numY, numX, ~, ~] = size(images);
     
+    % find 'most different (from median)' pixels in the stack of images
     subtractedImages = double(images) - double(gMedianImage);
-    distances = sum(subtractedImages.^2, 3);
-    [~, indexes] = max(distances, [], 4);
+    distances = sum(subtractedImages.^2, 3); % calculate differences
+    [~, indexes] = max(distances, [], 4);    % find indexes of 'most different' pixel
     
+    % convert 2D matrix index into linear index
     % https://stackoverflow.com/questions/57649949/indexing-a-4d-array-with-a-2d-matrix-of-indicies
-    linearIndex = reshape(1:numY*numX, numY, numX) ...
-        + reshape((0:2)*numY*numX, 1, 1, []) ...
-        + (indexes-1)*numY*numX*3;
+    numPixels = numY*numX;
+    linearIndex = reshape(1:numPixels, numY, numX) ...
+        + reshape((0:2)*numPixels, 1, 1, []) ...
+        + (indexes-1)*numPixels*3;
+    
+    % combine 'most different' pixels from images
     output = images(linearIndex);
 end
 
