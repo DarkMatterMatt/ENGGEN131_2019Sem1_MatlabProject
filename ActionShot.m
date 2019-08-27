b8cb48ac3beb140685855fa9a9e96cad1e543a39
DarkMatterMatt <mattmoran2000@gmail.com>
2019-08-27 13:45:35 +1200

    ActionShot and RemoveAction share the medianImage

--- ActionShot.m
+++ ActionShot.m
@@ -1,10 +1,20 @@
 function output = ActionShot(images)
-    global gMedianImage;
-    
     % convert to a 4D matrix - looks like
     %   [Y, X, RGB, N] where N is the image number
     images = cat(4, images{:});
     [numY, numX, ~, ~] = size(images);
+    
+    % ActionShot and RemoveAction both use the same median image so we save
+    %   it between functions
+    global gMedianImage;
+    
+    % if the images are different sizes then we changed datasets
+    % this will break if we change between multiple datasets with the same
+    %   number of pixels (but this shouldn't be a problem for this task)
+    if numel(gMedianImage) ~= numel(images)
+        % average matrix across the 4th dimension (stack of images)
+        gMedianImage = median(images, 4);
+    end
     
     % find 'most different (from median)' pixels in the stack of images
     subtractedImages = double(images) - double(gMedianImage);
--- RemoveAction.m
+++ RemoveAction.m
@@ -1,16 +1,21 @@
 function output = RemoveAction(images)
-    % https://stackoverflow.com/questions/5197597/how-to-average-over-a-cell-array-of-arrays
+    % Inspired by https://stackoverflow.com/questions/5197597/how-to-average-over-a-cell-array-of-arrays
     
-    % convert to a 4D matrix
-    images = cat(4, images{:});
-    
-    % average matrix across the 4th dimension (stack of images)
-    output = median(images, 4);
-    
-    % save so ActionShot can use result
+    % ActionShot and RemoveAction both use the same median image so we save
+    %   it between functions
     global gMedianImage;
-    gMedianImage = output;
     
-    % images are of the type uint8
-    output = uint8(output);
+    % if the images are different sizes then we changed datasets
+    % this will break if we change between multiple datasets with the same
+    %   number of pixels (but this shouldn't be a problem for this task)
+    if numel(gMedianImage) ~= numel(images)
+        % convert images to a 4D matrix (from cell array)
+        images = cat(4, images{:});
+
+        % average matrix across the 4th dimension (stack of images)
+        gMedianImage = median(images, 4);
+    end
+    
+    % return the median image
+    output = gMedianImage;
 end
