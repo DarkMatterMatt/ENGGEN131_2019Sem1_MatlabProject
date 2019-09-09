6c8d5e92e73d001d9bc41a489e71c280f6183f9d
DarkMatterMatt <mattmoran2000@gmail.com>
2019-09-09 13:34:03 +1200

    Prefix all test-related things with Test*

--- TestActionShot.m
+++ TestActionShot.m
@@ -10,6 +10,6 @@ imageNames = GenerateImageList(dirname, fileType);
 images = ReadImages(dirname, imageNames);
 
 % Create an action shot from the sequence of images
-assert(isequal(ActionShot(images), imread('peskyActionShot.png')));
+assert(isequal(ActionShot(images), imread('TestPeskyActionShot.png')));
 
 disp([testFunctionName ' passed all tests']);
--- TestRemoveAction.m
+++ TestRemoveAction.m
@@ -10,6 +10,6 @@ imageNames = GenerateImageList(dirname, fileType);
 images = ReadImages(dirname, imageNames);
 
 % Create an action shot from the sequence of images
-assert(isequal(RemoveAction(images), imread('peskyActionRemoved.png')));
+assert(isequal(RemoveAction(images), imread('TestPeskyActionRemoved.png')));
 
 disp([testFunctionName ' passed all tests']);
