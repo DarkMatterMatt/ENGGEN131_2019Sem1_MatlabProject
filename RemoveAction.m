6432ab7199c6eb411be491611cc00a93df94e2fc
DarkMatterMatt <mattmoran2000@gmail.com>
2019-09-12 14:00:30 +1200

    Remove risky optimisation
    Don't store the median image between tests

--- RemoveAction.m
+++ RemoveAction.m
@@ -5,22 +5,10 @@ function output = RemoveAction(images)
     % Author:   Matt Moran
     
     % Inspired by https://stackoverflow.com/questions/5197597/how-to-average-over-a-cell-array-of-arrays
-    
-    % ActionShot and RemoveAction both use the same median image so we save
-    %   it between functions
-    global gMedianImage;
-    
-    % if the images are different sizes then we changed datasets
-    % this will break if we change between multiple datasets with the same
-    %   number of pixels (but this shouldn't be a problem for this task)
-    if numel(gMedianImage) ~= numel(images{1})
-        % convert images to a 4D matrix (from cell array)
-        images = cat(4, images{:});
 
-        % average matrix across the 4th dimension (stack of images)
-        gMedianImage = median(images, 4);
-    end
-    
-    % return the median image
-    output = gMedianImage;
+    % convert images to a 4D matrix (from cell array)
+    images = cat(4, images{:});
+
+    % average matrix across the 4th dimension (stack of images)
+    output = median(images, 4);
 end
